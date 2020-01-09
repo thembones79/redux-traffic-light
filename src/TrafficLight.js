3d97047ca8310d44391af383087ba54b50ef67de
thembones79 <michal.szumnarski@gmail.com>
2020-01-09 23:46:58 +0100

    react version is working

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,15 +1,44 @@
 import React from "react";
+import "./TrafficLight.css";
 
 class TrafficLight extends React.Component {
+  state = { activeLight: "RED" };
+
+  changeLight = () => {
+    if (this.state.activeLight === "RED") {
+      this.setState({ activeLight: "YELLOW" });
+    }
+    if (this.state.activeLight === "YELLOW") {
+      this.setState({ activeLight: "GREEN" });
+    }
+    if (this.state.activeLight === "GREEN") {
+      this.setState({ activeLight: "RED" });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <div>
-          <div>R</div>
-          <div>Y</div>
-          <div>G</div>
+      <div className="traffic-light">
+        <div className="gasket">
+          <div
+            className={`light red ${
+              this.state.activeLight === "RED" ? "active" : ""
+            }`}
+          ></div>
+          <div
+            className={`light yellow ${
+              this.state.activeLight === "YELLOW" ? "active" : ""
+            }`}
+          ></div>
+          <div
+            className={`light green ${
+              this.state.activeLight === "GREEN" ? "active" : ""
+            }`}
+          ></div>
         </div>
-        <button>Next</button>
+        <button onClick={this.changeLight} className="next">
+          Next
+        </button>
       </div>
     );
   }
