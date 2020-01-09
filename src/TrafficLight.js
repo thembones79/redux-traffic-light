0eaa2cf5d69fa82a4ac550367fd6cf6ad9c9ed08
thembones79 <michal.szumnarski@gmail.com>
2020-01-09 16:43:35 +0100

    first commit

---
+++ src/TrafficLight.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class TrafficLight extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>R</div>
+          <div>Y</div>
+          <div>G</div>
+        </div>
+        <button>Next</button>
+      </div>
+    );
+  }
+}
+
+export default TrafficLight;
