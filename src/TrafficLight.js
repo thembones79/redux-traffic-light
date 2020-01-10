317357619ec9daf1e76e95fd0c29404eb5183e77
thembones79 <michal.szumnarski@gmail.com>
2020-01-10 22:05:16 +0100

    done redux version

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,47 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+import { RED, YELLOW, GREEN, changeLight } from "./actions";
 import "./TrafficLight.css";
 
-class TrafficLight extends React.Component {
-  state = { activeLight: "RED" };
-
-  changeLight = () => {
-    if (this.state.activeLight === "RED") {
-      this.setState({ activeLight: "YELLOW" });
-    }
-    if (this.state.activeLight === "YELLOW") {
-      this.setState({ activeLight: "GREEN" });
-    }
-    if (this.state.activeLight === "GREEN") {
-      this.setState({ activeLight: "RED" });
-    }
-  };
-
-  render() {
-    return (
-      <div className="traffic-light">
-        <div className="gasket">
-          <div
-            className={`light red ${
-              this.state.activeLight === "RED" ? "active" : ""
-            }`}
-          ></div>
-          <div
-            className={`light yellow ${
-              this.state.activeLight === "YELLOW" ? "active" : ""
-            }`}
-          ></div>
-          <div
-            className={`light green ${
-              this.state.activeLight === "GREEN" ? "active" : ""
-            }`}
-          ></div>
-        </div>
-        <button onClick={this.changeLight} className="next">
-          Next
-        </button>
+const TrafficLight = ({ activeLight, changeLight }) => {
+  return (
+    <div className="traffic-light">
+      <div className="gasket">
+        <div
+          className={`light red ${activeLight === RED ? "active" : ""}`}
+        ></div>
+        <div
+          className={`light yellow ${activeLight === YELLOW ? "active" : ""}`}
+        ></div>
+        <div
+          className={`light green ${activeLight === GREEN ? "active" : ""}`}
+        ></div>
       </div>
-    );
-  }
+      <button onClick={changeLight} className="next">
+        Next
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return { activeLight: state.activeLight };
 }
 
-export default TrafficLight;
+export default connect(mapStateToProps, { changeLight })(TrafficLight);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import TrafficLight from "./TrafficLight";
+import { RED, YELLOW, GREEN, CHANGE_LIGHT } from "./actions";
+
+const initialState = { activeLight: RED };
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_LIGHT:
+      switch (state.activeLight) {
+        case RED:
+          return { activeLight: YELLOW };
+        case YELLOW:
+          return { activeLight: GREEN };
+        case GREEN:
+          return { activeLight: RED };
+        default:
+          return state;
+      }
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 const App = () => {
   return <TrafficLight />;
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
